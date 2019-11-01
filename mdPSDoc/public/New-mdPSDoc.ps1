aee13433f638e75ab423791451040882e73b24c2
Michal Zindulka <michal.zindulka@gmail.com>
2019-11-01 15:09:04 +0100

    Help added.

--- mdPSDoc/public/New-mdPSDoc.ps1
+++ mdPSDoc/public/New-mdPSDoc.ps1
@@ -1,4 +1,35 @@
-function New-mdPSDoc {
+<#
+.SYNOPSIS
+    Generates a Markdown help file.
+.DESCRIPTION
+    This function generates help file in Markdown format, which can be then posted on Website supporting Markdown language e.g. GitHub.
+.PARAMETER HelpObject
+    Use to generate markdown help from a help object created using Get-Help -Full cmdlet.
+.PARAMETER OutputLocation
+    Defines the output location. If not specified, output is generated to current folder.
+.PARAMETER OutputToHost
+    Output to host only.
+.PARAMETER CommandName
+    Use to generate markdown help from a cmdlet.
+.PARAMETER Module
+    Use to generate markdown help from an entire module.
+.PARAMETER File
+    Use to generate markdown help from a PowerShell script file.
+.EXAMPLE
+    New-MDPSDoc -CommandName Get-Service
+    Generate Markdown help from a cmdle 'Get-Service' and save to current location.
+.EXAMPLE
+    New-MDPSDoc -File /Users/mike/Documents/Scripts/PowerShell/Azure/Get-AzVmNetwork/Get-AzVmNetwork.ps1 -OutputLocation /Users/mike/Documents/Scripts/HelpFiles/
+    Generate Markdown help from a PowerShell script file and store to defined output location.
+.EXAMPLE
+    New-MDPSDoc -Module mzToolkit.General
+    Generate Markdown help from each cmdlet of module and store to current location.
+.NOTES
+    Use with joy.
+.LINK
+    https://github.com/michalzindulka/mdPSDoc
+#>
+function New-MDPSDoc {
     [CmdletBinding(DefaultParameterSetName='command')]
     param (
         # Input Help Object:
